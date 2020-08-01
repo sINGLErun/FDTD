16a295dba652bbbfc3e98903e887d8127290de6c
sINGLERun <crimsonschist@gmail.com>
2020-08-01 17:50:58 +0700

    story 1: first tries of FDTD

---
+++ 1Dimentional_solution.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cassert>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+
+const unsigned SIZE_X = 200;
+const unsigned TIME_m = 1000;
+const unsigned DET_POS = 50;
+const unsigned SRC_POS = 40;
+const std::string det_path = "det._values.txt";
+const std::string source_path = "source_values.txt";
+const float W0 = 120*3.14159, Sc = 1.0;
+
+void component_init (float*, float*, unsigned SIZE);
+
+int main(int argc, char const *argv[])
+{
+	float Hy[SIZE_X], Ez[SIZE_X];
+	std::ofstream fout_det, fout_source;
+
+	component_init(Hy, Ez, SIZE_X);
+
+	fout_det.open(det_path);
+	assert(fout_det.is_open() && "det._values.txt wasn't open for writing");
+	fout_det << TIME_m << "\n";
+	fout_det << std::setprecision(6) << std::fixed;
+
+
+	fout_source.open(source_path);
+	assert(fout_source.is_open() && "source_values.txt wasn't open for writing");
+	fout_source << TIME_m << "\n";
+	fout_source << std::setprecision(6) << std::fixed;
+	
+	for (int t = 0; t < TIME_m; ++t) {
+
+		// find Hy[] at t+1/2
+		Hy[SIZE_X-1] = Hy[SIZE_X-2]; // absorbing boundary conditions
+		for (int i = 0; i < SIZE_X-1; ++i) // x = i*dx;
+			Hy[i] += (Ez[i+1] - Ez[i])*Sc / W0;
+
+		Hy[SRC_POS - 1] += -exp(-(t - 30.0)*(t - 30.0)/100.0);
+		
+		// find Ez[] at t
+		Ez[0] = Ez[1]; // absorbing boundary conditions
+		for (int i = 1; i < SIZE_X; ++i)
+			Ez[i] += (Hy[i] - Hy[i-1])*Sc * W0;
+		
+		Ez[SRC_POS] += exp(-(t+1 - 30.0)*(t+1 - 30.0)/100.0);
+
+		fout_source << Ez[SRC_POS] << "\n";
+		fout_det << Ez[DET_POS] << "\n";
+
+	}
+	fout_det.close();
+	fout_source.close();
+
+	return 0;
+}
+
+void component_init(float* comp1_, float* comp2_, unsigned SIZE) {
+	for (int i = 0; i < SIZE; ++i) {
+		comp1_[i] = 0;
+		comp2_[i] = 0;
+	}
+}
